1e9337abff27af89a5775b7478c87043a196b630
Austin Hubler <austinjacob2010@icloud.com>
2026-02-25 16:18:08 -0600

    Subsystem start

--- subsystems/Launcher.py
+++ subsystems/Launcher.py
@@ -5,13 +5,14 @@ from commands2 import subsystem
 from ntcore.util import ntproperty 
 
 from phoenix6.hardware import TalonFX # Motor controller class for Falcons & Krakens
+from phoenix6.configs import TalonFXConfiguration, MotorOutputConfigs, Slot0Configs
 
 class Launcher(subsystem):
   
     class Speeds:
-        STOP: 0
+        STOP: 0.0
         SLOW: float = -0.1
-        FULL: float = -33
+        FULL: float = -33.0
 
     class LauncherConstraints:
         kP = 0.2
@@ -36,6 +37,17 @@ class Launcher(subsystem):
 
     def __init__(self, motor_port:int):
         self.flywheel = TalonFX(motor_port)
+
+        self.config = TalonFXConfiguration()
+        self.flywheelConfig = self.config.with_motor_output(MotorOutputConfigs).with_slot0(
+            Slot0Configs()
+                .with_k_p(self.LauncherConstraints.kP)
+                .with_k_i(self.LauncherConstraints.kI)
+                .with_k_d(self.LauncherConstraints.kD)
+                .with_k_v(self.LauncherConstraints.kV)
+                .with_k_s(self.LauncherConstraints.kS)
+                .with_k_a(self.LauncherConstraints.kA)
+        )
 
         
     def periodic(self):
